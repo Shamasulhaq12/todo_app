f8f7bbf60e2cb9a65c0002d809d05c475c51d352
shamsulhaq <shamsulhaq@beyonderissolutions.com~>
2023-01-22 18:53:35 +0500

    Task is completed

--- server/app/routes.py
+++ server/app/routes.py
@@ -3,5 +3,5 @@ from flask_restful import Api
 from .views import TaskAPIView, RetrieveTaskView
 
 api = Api(app)
-api.add_resource(TaskAPIView, '/task/')
+api.add_resource(TaskAPIView, '/tasks/')
 api.add_resource(RetrieveTaskView, '/task/<task_id>/')
